795c4a7628c9fc2173ea01dd30926cd00eab1d6a
Enrique-at-isco <Enrique.Farias@isco-pipe.com>
2024-12-09 09:38:33 -0600

    Add files via upload

--- python_Arduino_cloud.py
+++ python_Arduino_cloud.py
@@ -1,7 +1,8 @@
 import time
 import logging
-
+import time
 import sys
+import RPi.GPIO as GPIO
 sys.path.append("lib")
 
 from arduino_iot_cloud import ArduinoCloudClient
@@ -19,6 +20,34 @@ def logging_func():
 # This function is executed each time the "test_switch" variable changes 
 def on_switch_changed(client, value):
     print("Switch Pressed! Status is: ", value)
+
+def read_GPIO():
+
+    # Set up GPIO
+    GPIO.setmode(GPIO.BCM)  # Use BCM numbering
+    GPIO.setwarnings(False)
+
+    # Define pin number
+    GPIO_PIN = 18
+
+    # Set up the GPIO pin as an input
+    GPIO.setup(GPIO_PIN, GPIO.IN)
+
+    # read and print gpio pin value
+    try:
+        while True:
+            # Read the state of the pin
+            pin_value = GPIO.input(pin_number)
+
+            print("Pin {} value: {}".format(pin_number, pin_value))
+
+            time.sleep(1)  # Add a delay
+    # Clean up
+    except KeyboardInterrupt:
+    # Cleanup GPIO on exit
+     GPIO.cleanup()
+
+
 
 if __name__ == "__main__":
 
